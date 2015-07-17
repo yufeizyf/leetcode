ed99f90d4546afd8b2598eb30a89811860b08b57
yufeizyf <yufei.zyf@alibaba-inc.com>
2015-07-17 21:46:54 +0800

    Update Solution.java

--- [238]Product of Array Except Self/Solution.java
+++ [238]Product of Array Except Self/Solution.java
@@ -4,11 +4,13 @@ public class Solution {
     	
     	int[] output = new int[nums.length];
     	
+    	//Step1: make output[i] = nums[i] * nums[i-1] * ... * nums0 and set output[0] = 1. N is length of nums
     	output[0] = 1;
     	for(int i = 1; i < output.length; i++) {
     		output[i] = output[i-1] * nums[i-1];
     	}
     	
+    	//Step2: make nums[i] = nums[i] * nums[i+1] * ... * numsN-1 and set nums[N] = 1
     	int right = 1;
     	int temp = 1;
     	for(int i = nums.length - 1; i >= 0; i--) {
@@ -17,6 +19,7 @@ public class Solution {
     		right = temp * nums[i];
     	}
     	
+    	//Step3: output[i] = output[i] * nums[i]
     	for(int i = 0; i < output.length; i++) {
     		output[i] = nums[i] * output[i];
     	}
